389203ce2f0fcc0885a952b002a7b5099dc84b1f
Mizll <ngoquocminh03hh3@gmail.com>
2025-11-17 12:10:48 +0700

    testdb

--- obj/Debug/net8.0/khoaluantotnghiep.AssemblyInfo.cs
+++ obj/Debug/net8.0/khoaluantotnghiep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("khoaluantotnghiep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2304c2bfeeee61747ffaf680098d1c5dcd41058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a576ce2700fdfaf3ca81f66f2e776a23fb9fae4")]
 [assembly: System.Reflection.AssemblyProductAttribute("khoaluantotnghiep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("khoaluantotnghiep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
